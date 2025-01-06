3e9a06029093e10d998f47caaa5c902033b67da5
hidetsumi <hidetsu99@gmail.com>
2025-01-06 16:59:48 -0300

    fix: fix the initalization of pumpinks, was duplicated

--- proyectos-live/booscaminas/src/components/Booscaminas.tsx
+++ proyectos-live/booscaminas/src/components/Booscaminas.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { useCallback, useMemo } from "react";
 
 interface Casilla {
   position: number;
@@ -12,14 +12,30 @@ interface BooscaminasProp {
 }
 
 export const Booscaminas = ({
-  pumpkins = 3,
+  pumpkins = 10,
   enableFlags = false,
   gridDimension = 10,
 }: BooscaminasProp) => {
   const initialGameState = useMemo(() => {
-    const pumpkinsInitialState = new Array(pumpkins)
-      .fill(0)
-      .map(() => Math.floor(Math.random() * (gridDimension * gridDimension)));
+    const pumpkinsInitialState = [...Array(Math.pow(gridDimension, 2)).keys()]
+      .sort(() => Math.random() - 0.5)
+      .slice(0, pumpkins);
+
+    // Array.from({ length: pumpkins }, (_, i) => i).sort(
+    // () => Math.random() * (gridDimension * gridDimension),
+    // );
+
+    console.log(pumpkinsInitialState);
+    //   [
+    //   ...new Set(
+    //     Array(pumpkins)
+    //       .fill(0)
+    //       .map((_, i) => i),
+    //   ),
+    // ];
+    // new Array(pumpkins)
+    //   .fill(0)
+    //   .map(() => Math.floor(Math.random() * (gridDimension * gridDimension)));
 
     const initialGameState = new Array(gridDimension * gridDimension)
       .fill(0)
@@ -35,12 +51,35 @@ export const Booscaminas = ({
 
   console.log([...initialGameState.filter((pump) => pump.active)]);
 
+  const getSurroundingCells = useCallback(
+    ({ active, position }: Casilla) => {
+      console.log(position);
+      const topLine = [
+        position - gridDimension - 1,
+        position - gridDimension,
+        position - gridDimension + 1,
+      ];
+
+      const innerLine = [position - 1, position, position + 1];
+
+      const bottomLine = [
+        position + gridDimension - 1,
+        position + gridDimension,
+        position + gridDimension + 1,
+      ];
+
+      console.log([...topLine, ...innerLine, ...bottomLine].filter((num) => num > 0));
+    },
+    [gridDimension],
+  );
+
   return (
     <div className={`grid grid-cols-${gridDimension} gap-1`}>
       {initialGameState.map((grid, i) => (
         <div
           key={i}
-          className="flex justify-center items-center w-[50px] h-[50px] border border-orange-400"
+          className={`flex justify-center items-center w-[50px] h-[50px] border border-orange-400 ${grid.active ? "bg-orange-400" : ""}`}
+          onClick={() => getSurroundingCells(grid)}
         >
           {grid.active} {grid.position}
         </div>
